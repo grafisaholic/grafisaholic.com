18be863b7ade31dc1724ad3980a83a5103538914
grafisaholic <khabiburrokhman13@gmail.com>
2021-08-13 02:31:24 +0700

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,54 @@
-import '../styles/globals.css'
+import { ChakraProvider, ColorModeProvider, useColorMode } from '@chakra-ui/react'
+import CustomTheme from '../styles/theme'
+import {Global, css} from '@emotion/react'
+
+const GlobalStyle = ({ children }) => {
+  const { colorMode } = useColorMode()
+
+  return (
+    <>
+      <Global
+        styles={css`
+          ::selection {
+            background-color: #90CDF4;
+            color: #fefefe;
+          }
+          ::-moz-selection {
+            background: #ffb7b7;
+            color: #fefefe;
+          }
+          html {
+            min-width: 356px;
+            scroll-behavior: smooth;
+          }
+          #__next {
+            display: flex;
+            flex-direction: column;
+            min-height: 100vh;
+            background: ${colorMode === 'light' ? 'white' : '#171717'};
+          }
+        `}
+      />
+      {children}
+    </>
+  )
+}
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider resetCSS theme={CustomTheme}>
+      <ColorModeProvider
+        options={{
+          initialColorMode: "light",
+          useSystemColorMode: true
+        }}
+      >
+        <GlobalStyle>
+          <Component {...pageProps} />
+        </GlobalStyle>
+      </ColorModeProvider>
+    </ChakraProvider>
+  ) 
 }
 
 export default MyApp
